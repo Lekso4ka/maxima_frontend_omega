ced9197d8691533e8a45eda6786b1dcd354bf61b
Leksa <admin@petiteweb.ru>
2023-10-04 22:29:24 +0300

    Изменила шапку сайта (добавила навигацию)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-//import './style/style.scss'
+import {Link} from "react-router-dom";
+import Logo from "../ui/Logo";
+import {useDispatch} from "react-redux";
+import {logOut} from "../../core/store/features/user/userSlice";
+import {LogoutOutlined} from "@ant-design/icons";
+import {Button} from "antd";
+import './style.scss'
 
+function Nav(){
+      return <nav>
+            <Link to="/">Главная</Link>
+            <Link to="/users">Пользователи</Link>
+            <Link to="/news">Новости</Link>
+            <Link to="/materials">Материалы</Link>
+      </nav>
+}
 
 
 const Header = () => {
-return (
+      const dispatch = useDispatch()
+      const logOutHandler = () => dispatch(logOut())
+      return (
 
-    <header> 
-<div className="header__logo">  </div>
-< h2>Учебный центр Maxima</h2>
+          <header>
+                <Logo/>
+                <Nav/>
+                <Button onClick={logOutHandler} icon={<LogoutOutlined />}/>
 
-      </header>
-)
+          </header>
+      )
 
 }
 
