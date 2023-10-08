fe3c8688036265d5cf331c1a187acf4b46b56c10
Муса Сулейманов <busalbanohcho@mail.ru>
2023-10-08 14:08:25 +0300

    Сохранение файлов перед Merge

--- src/components/ui/CardNews/CardNews.jsx
+++ src/components/ui/CardNews/CardNews.jsx
@@ -1,12 +1,12 @@
 import './CardNews.scss'
 
-export default function CardNews({ myNew }) {
+export default function CardNews({ url, data, title }) {
     return (
         <div className="CardNews1">
-            <img className="CardNews_image" src={myNew.url} alt="" />
+            <img className="CardNews_image" src={url} alt="" />
             <div className="CardNews2">
-                <p className="CardNews_data">{myNew.date}</p>
-                <h2 className="CardNews_title">{myNew.title}</h2>
+                <p className="CardNews_data">{data}</p>
+                <h2 className="CardNews_title">{title}</h2>
             </div>
             <button className="CardNews_button">Читать далее</button>
         </div>
