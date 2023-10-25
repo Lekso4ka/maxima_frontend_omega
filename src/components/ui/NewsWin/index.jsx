8e54f1ff38369a0097e581e9dc19b7df92761e78
Rshae <rshaevich@mail.ru>
2023-10-25 22:13:33 +0300

    Work in progress form and modal news win

--- src/components/ui/NewsWin/index.jsx
+++ src/components/ui/NewsWin/index.jsx
@@ -1,27 +1,33 @@
 import { Input, Modal, Space, Checkbox, Image } from 'antd';
 import TextArea from "antd/es/input/TextArea";
 import { useState } from "react";
+import ModalWin from "../ModalWin/ModalWin";
+
 
 const NewsWin = ({ isNewsWinOpen, setIsNewsWinOpen }) => {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
-
+    const [titleImage, setTitleImage] = useState('');
+    const [data, setData] = useState({});
 
 
     const onChange = (e) => {
         console.log(`checked = ${e.target.checked}`);
     };
 
-    const dataNews = {
-        title: "ПРОШЛА КОНФЕРЕНЦИЯ MAXIMA SCHOOL1",
-        titleImage: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-        forStudents: true,
-        forTeachers: false,
-        text: "На конференции компания MAXIMA SCHOOL продемонстрировала 11 экспериментальных инструментов и функций. Это новые творческие возможности использования новых технологий генеративного ИИ и 3D",
-        createdAt: "2023-10-21"
-    };
 
-    const handleNewsWinOk = () => {
+
+    const handleNewsWinOk = (e) => {
+        e.preventDefault();
+        const dataNews = {
+            title: title,
+            titleImage: titleImage,
+            forStudents: true,
+            forTeachers: false,
+            text: text,
+            createdAt: new Date().toISOString()
+        };
+        setData(dataNews)
         console.log(dataNews)
     };
 
@@ -30,15 +36,20 @@ const NewsWin = ({ isNewsWinOpen, setIsNewsWinOpen }) => {
     };
     return (
         <>
-            <Modal title="Title" open={isNewsWinOpen} onOk={handleNewsWinOk} onCancel={handleNewsWinCancel}>
-                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                    <Input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                    <Image width={300} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
-                    <Checkbox onChange={onChange}>forStudents</Checkbox>
-                    <Checkbox onChange={onChange}>forTeachers</Checkbox>
+            {/* <Modal title="Title" open={isNewsWinOpen} onOk={handleNewsWinOk} onCancel={handleNewsWinCancel}>
+                <Space direction="vertical" size="middle" style={{ display: 'flex' }}> */}
+            <ModalWin setOpenModal={setIsNewsWinOpen}>
+                <form onSubmit={handleNewsWinOk}>
+                    <input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                    <input type="url" value={titleImage} src=" " placeholder="Image" />
+                     <Checkbox onChange={onChange}>forStudents</Checkbox>
+                <Checkbox onChange={onChange}>forTeachers</Checkbox> 
                     <TextArea rows={4} placeholder="Текст" value={text} onChange={(e) => setText(e.target.value)} />
-                </Space>
-            </Modal>
+                    <button>Добавить</button>
+                </form>
+            </ModalWin>
+            {/*      </Space>
+            </Modal> */}
         </>
     );
 };
