29e5c094bcf08a01edfea4a8b5205b112fc153f7
Leksa <admin@petiteweb.ru>
2023-11-11 21:29:48 +0300

    Доработали добавление новости

--- src/components/ui/NewsWin/index.jsx
+++ src/components/ui/NewsWin/index.jsx
@@ -5,6 +5,9 @@ import { useState } from "react";
 const NewsWin = ({ isNewsWinOpen, setIsNewsWinOpen }) => {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
+    const [teacherFlag, setTeacherFlag] = useState(true);
+    const [studentFlag, setStudentFlag] = useState(true);
+    const [img, setImg] = useState("");
 
 
 
@@ -12,17 +15,32 @@ const NewsWin = ({ isNewsWinOpen, setIsNewsWinOpen }) => {
         console.log(`checked = ${e.target.checked}`);
     };
 
-    const dataNews = {
-        title: "ПРОШЛА КОНФЕРЕНЦИЯ MAXIMA SCHOOL1",
-        titleImage: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-        forStudents: true,
-        forTeachers: false,
-        text: "На конференции компания MAXIMA SCHOOL продемонстрировала 11 экспериментальных инструментов и функций. Это новые творческие возможности использования новых технологий генеративного ИИ и 3D",
-        createdAt: "2023-10-21"
-    };
+    // const dataNews = {
+    //     title: "ПРОШЛА КОНФЕРЕНЦИЯ MAXIMA SCHOOL1",
+    //     titleImage: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    //     forStudents: true,
+    //     forTeachers: false,
+    //     text: "На конференции компания MAXIMA SCHOOL продемонстрировала 11 экспериментальных инструментов и функций. Это новые творческие возможности использования новых технологий генеративного ИИ и 3D",
+    //     createdAt: "2023-10-21"
+    // };
 
     const handleNewsWinOk = () => {
-        console.log(dataNews)
+        // console.log(dataNews)
+        const result = {
+            title: title,
+            titleImage: img,
+            forStudents: studentFlag,
+            forTeachers: teacherFlag,
+            text: text,
+            createdAt: new Date().toISOString()
+        }
+        console.log(result)
+        setIsNewsWinOpen(false);
+        setTitle("")
+        setImg("")
+        setText("")
+        setStudentFlag(true)
+        setTeacherFlag(true)
     };
 
     const handleNewsWinCancel = () => {
@@ -33,9 +51,13 @@ const NewsWin = ({ isNewsWinOpen, setIsNewsWinOpen }) => {
             <Modal title="Title" open={isNewsWinOpen} onOk={handleNewsWinOk} onCancel={handleNewsWinCancel}>
                 <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
                     <Input placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                    <Image width={300} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
-                    <Checkbox onChange={onChange}>forStudents</Checkbox>
-                    <Checkbox onChange={onChange}>forTeachers</Checkbox>
+                    <Image width={300} src={img} />
+                    <Input placeholder="Image" value={img} onChange={(e) => setImg(e.target.value)} />
+                    <Checkbox checked={studentFlag} onChange={e => setStudentFlag(e.target.checked)}>forStudents</Checkbox>
+                    <Checkbox 
+                        checked={teacherFlag} 
+                        onChange={e => setTeacherFlag(!teacherFlag)}
+                    >forTeachers</Checkbox>
                     <TextArea rows={4} placeholder="Текст" value={text} onChange={(e) => setText(e.target.value)} />
                 </Space>
             </Modal>
