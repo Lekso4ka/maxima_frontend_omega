be9ff3cf7f78e2364c386024e8a8a303c7cbd8e3
Rshae <rshaevich@mail.ru>
2023-11-18 23:15:30 +0300

    in the beginning

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -2,13 +2,47 @@ import React, {useState} from 'react';
 import Title from "antd/lib/typography/Title";
 import {Button, Space} from "antd";
 import NewsWin from "../../components/ui/NewsWin";
-
+import $api from '../../core/http';
+import { useEffect } from "react";
 
 const AddNews = () => {
       const [isNewsWinOpen, setIsNewsWinOpen] = useState(false);
     const showNewsWinModal = () => {
         setIsNewsWinOpen (true);
     };
+    const [news, setNews] = useState ([]);
+    const [ns, setNs] = useState ("");
+
+    useEffect (() => {
+ $api.get("/news/{id}")
+ .then(result => {
+   console.log(result.data.data);
+   setNews(result.data.data)
+    })
+}, [])
+
+useEffect (() => {
+    $api.delete("/news/{id}")
+    .then(result => {
+      console.log(result.data);
+      setNews(result.data)
+       })
+    
+      }, [])
+
+      const newsHandler = async () => {
+        const result = await $api.post("/news/{id}",{name: ns})
+        .then(result =>{
+        console.log(result)
+        
+        })
+         
+        console.log(result);
+        setNews(prev => [...prev, result.data])
+        setNs("")
+        }
+        
+
 
    return (
         <>
