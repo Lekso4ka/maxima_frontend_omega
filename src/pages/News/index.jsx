df780ee647b56d495fd26d20e7c6a9a89ac1a860
Leksa <admin@petiteweb.ru>
2023-11-19 00:11:48 +0300

    finish news for practise! =)

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,34 +1,39 @@
 import React, {useState} from 'react';
 import Title from "antd/lib/typography/Title";
+import { useSelector, useDispatch } from 'react-redux';
 import {Button, Space} from "antd";
 import NewsWin from "../../components/ui/NewsWin";
 import $api from '../../core/http';
 import { useEffect } from "react";
+import {getNews, dellNews} from "../../core/store/features/news/newsSlice";
 
 const AddNews = () => {
-      const [isNewsWinOpen, setIsNewsWinOpen] = useState(false);
+    const [isNewsWinOpen, setIsNewsWinOpen] = useState(false);
     const showNewsWinModal = () => {
         setIsNewsWinOpen (true);
     };
-    const [news, setNews] = useState ([]);
+    const news = useSelector(state => state.news.data);
+    const dispatch = useDispatch();
+    // const [news, setNews] = useState ([]);
     const [ns, setNs] = useState ("");
 
-    useEffect (() => {
- $api.get("/news/{id}")
+useEffect (() => {
+ $api.get("/news")
  .then(result => {
    console.log(result.data.data);
-   setNews(result.data.data)
+   dispatch(getNews(result.data.data));
+//    setNews(result.data.data)
     })
 }, [])
 
-useEffect (() => {
-    $api.delete("/news/{id}")
-    .then(result => {
-      console.log(result.data);
-      setNews(result.data)
-       })
+// useEffect (() => {
+//     $api.delete("/news/{id}")
+//     .then(result => {
+//       console.log(result.data);
+//       setNews(result.data)
+//        })
     
-      }, [])
+//     }, [])
 
       const newsHandler = async () => {
         const result = await $api.post("/news/{id}",{name: ns})
@@ -38,10 +43,21 @@ useEffect (() => {
         })
          
         console.log(result);
-        setNews(prev => [...prev, result.data])
+        // setNews(prev => [...prev, result.data])
         setNs("")
         }
         
+        const delHandler = (id) => {
+            $api.delete(`/news/${id}`)
+                .then(result => {
+                    if (result.status === 200) {
+                        console.log(result.data);
+                        // setNews(result.data)
+                        dispatch(dellNews(id));
+                    }
+                })
+            // console.log(id);
+        }
 
 
    return (
@@ -53,6 +69,23 @@ useEffect (() => {
                 </Button>
             </Space>
                   <NewsWin isNewsWinOpen={isNewsWinOpen} setIsNewsWinOpen={setIsNewsWinOpen}/>
+
+            {news?.length > 0 && <table>
+                <tr>
+                    <th>id</th>
+                    <th>Заголовок</th>
+                    <th>Дата создания</th>
+                    <th>Новость</th>
+                    <th>Управление</th>
+                </tr>
+                {news.map(el => <tr>
+                    <td>{el.id}</td>
+                    <td>{el.title}</td>
+                    <td>{el.createdAt}</td>
+                    <td>{el.text}</td>
+                    <td><button onClick={e => delHandler(el.id)}>del</button></td>
+                </tr>)}
+            </table>}
         </>
     );
 };
